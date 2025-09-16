4315602007bae09bae0681a3599f905ebd5dc5e0
admin <npruitt@uark.edu>
2025-09-16 14:57:47 -0500

    Began creating db where you can add players

---
+++ database.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+# define connector
+def connect_data():
+    return sqlite3.connect('photon.db')
+
+# define cursor
+def init_db():
+    con = connect_data()
+    cursor = con.cursor()
+
+    # create table
+    command = """CREATE TABLE IF NOT EXISTS players(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, score INTEGER DEFAULT 0) """
+
+    cursor.execute(command)
+
+    con.commit()
+    con.close()
+
+def list_players():
+        con = connect_data()
+        cursor = con.cursor()
+        cursor.execute("SELECT id, name, score FROM players")
+        rows = cursor.fetchall()
+        con.close()
+        return rows
+
+def clear_players():
+     con = connect_data()
+     cursor = con.cursor()
+     cursor.execute("DELETE FROM players")
+     con.commit()
+     con.close()
+
+
+
+
+
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 
 from src.views.loading_screen import LoadingScreen
 from src.constants import ScreenConstants
+from database import connect_data, init_db, list_players, clear_players
 
 
 GAME_TITLE = "Photon"
@@ -19,6 +20,22 @@ def main():
     loading_screen = LoadingScreen(screen)
     loading_screen.load_starting_screen()
 
+    # add players
+    def add_player(name):
+        con = connect_data()
+        cursor = con.cursor()
+        cursor.execute("INSERT INTO players (name) VALUES (?)",(name, ))
+        con.commit()
+        con.close()
+    
+    # Test adding players
+    clear_players()
+    add_player("Jane")
+    add_player("Bob")
+    print("Players: ")
+    for player in list_players():
+        print(player)
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -29,4 +46,5 @@ def main():
     pygame.quit()
 
 if __name__ == "__main__":
+    init_db()
     main()
