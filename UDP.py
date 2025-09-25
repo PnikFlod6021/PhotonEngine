eb0a90c288fd5a85b29954457b1b129402be06ca
Pouya Sohrabipour <88556070+pooyasohrabipoor@users.noreply.github.com>
2025-09-25 14:32:52 -0500

    Update UDP.py

--- UDP.py
+++ UDP.py
@@ -1,7 +1,7 @@
 import socket
 
 HOST = "127.0.0.1"   # IP address
-TX_PORT = 7500       #  transmit on 7501
+TX_PORT = 7500       #  transmit on 7500
 
 # Create UDP TX socket
 tx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -14,10 +14,17 @@ rx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 rx_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 rx_sock.bind(("0.0.0.0", RX_PORT))
 ### The code will stop here until something is sent to RX_PORT
+# if data is 202 it means the game is about to start and start trabsmitting equipment codes
 print("listening")
-data, addr = rx_sock.recvfrom(BUF_SIZE)  # blocks until something arrives
-text = data.decode("utf-8").strip()
-print(f"Received from {addr}: {text}") ### Prints the Received Data
+received_data = ' '
+while received_data != '202':
+	print("data was not 202")
+	data, addr = rx_sock.recvfrom(BUF_SIZE) 
+	received_data = data.decode('utf-8')
+	print ("Received from game software: " + received_data)
+print ('')
+
+
 
 ###### This is a while True Loop that keeps checking for your input
 # take your input and send data to TX_PORT
