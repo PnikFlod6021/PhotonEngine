f5bdeb14f76318496687d6857a8938a4d4ddf8ec
Rithvik Vannemreddy <113538025+PnikFlod6021@users.noreply.github.com>
2025-10-28 14:42:12 -0500

    Changed Game Start Broadcast to 7500

--- src/models/UDP/UDP_client.py
+++ src/models/UDP/UDP_client.py
@@ -10,8 +10,8 @@ def broadcast_equipment_id(equipment_id):
 
 def broadcast_message(message):
     tx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    tx_sock.sendto(str(message).encode("utf-8"), (UDPConstants.HOST, UDPConstants.RX_PORT))
-    print(f"Sent start message: {message} to {UDPConstants.HOST}:{UDPConstants.RX_PORT}")
+    tx_sock.sendto(str(message).encode("utf-8"), (UDPConstants.HOST, UDPConstants.TX_PORT))
+    print(f"Sent start message: {message} to {UDPConstants.HOST}:{UDPConstants.TX_PORT}")
     tx_sock.close()
 
 
